56df4eef300f43c1ccd1c2729aab37c63ed8482c
edawg <muhammadridho5654@gmail.com>
2026-04-22 10:16:52 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,33 @@ import RootLayout from './components/layouts/RootLayout';
 import { useAuth } from './context/AuthContext';
 import { AuthProvider } from './providers/AuthProvider';
 import { QueryProvider } from './providers/QueryProvider';
-
-const Login = lazy(() => import('./pages/Login'));
-
-const Dashboard = lazy(() => import('./pages/Dashboard'));
-const AttendanceRecap = lazy(() => import('./pages/Attendance/AttendanceRecap'))
-
-const KaryawanList = lazy(() => import('./pages/Karyawan/KaryawanList'));
-
-
+const lazyWithRetry = (componentImport: () => Promise<any>) =>
+  lazy(async () => {
+    const pageHasAlreadyBeenForceRefreshed = JSON.parse(
+      window.sessionStorage.getItem('page-has-been-force-refreshed') || 'false'
+    );
+    try {
+      const component = await componentImport();
+      window.sessionStorage.setItem('page-has-been-force-refreshed', 'false');
+      return component;
+    } catch (error) {
+      if (!pageHasAlreadyBeenForceRefreshed) {
+        window.sessionStorage.setItem('page-has-been-force-refreshed', 'true');
+        window.location.reload();
+        return new Promise(() => { });
+      }
+      throw error;
+    }
+  });
+const Login = lazyWithRetry(() => import('./pages/Login'));
+const Dashboard = lazyWithRetry(() => import('./pages/Dashboard'));
+const AttendanceRecap = lazyWithRetry(() => import('./pages/Attendance/AttendanceRecap'));
+const KaryawanList = lazyWithRetry(() => import('./pages/Karyawan/KaryawanList'));
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   const { isAuthenticated } = useAuth();
   if (!isAuthenticated) return <Navigate to="/login" replace />;
   return <>{children}</>;
 };
-
 const App = () => {
   return (
     <QueryProvider>
@@ -28,7 +40,6 @@ const App = () => {
           <Suspense fallback={<PageLoader />}>
             <Routes>
               <Route path="/login" element={<Login />} />
-
               <Route path="/" element={<ProtectedRoute><RootLayout /></ProtectedRoute>}>
                 <Route index element={<Dashboard />} />
                 <Route path="attendance/recap" element={<AttendanceRecap />} />
@@ -41,5 +52,4 @@ const App = () => {
     </QueryProvider>
   );
 };
-
 export default App;
